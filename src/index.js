38598211fbf42c79cbdf1bd2f199209843e8f039
Jessica Kavanagh <kavanagh.jc@gmail.com>
2025-12-09 16:26:20 -0600

    index.js | basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Dashboard from "./pages/Dashboard";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
